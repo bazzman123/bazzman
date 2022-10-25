7a066ae5ec09ed660e234dc3bfee69adb312ac39
bazzman123 <116677511+bazzman123@users.noreply.github.com>
2022-10-25 22:18:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -72,7 +72,6 @@ function getCARS(link) {
       console.log(doc);
       let carLinks = doc.getElementsByClassName("clickable-tr").childNodes[0];
       console.log(carLinks);
-      console.log("hhh")
       for (let i = 0; i < 3; i++) {
           console.log(carLinks[i]);
       };
