c91bd9234e915950f590b8e933cba6a5fbee2422
bazzman123 <116677511+bazzman123@users.noreply.github.com>
2022-10-25 21:30:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,11 +24,9 @@ function getHTML(link) {
       console.log(doc);
       let href = doc.getElementsByClassName("link-primary")[0].href;
       console.log("person href:", href);
-      info.push(
-          "searchedLink": href
-      );
+      info."searchedLink": href;
       console.log("stored:", info);
-      getCODE(href)
+      getCODE(href);
   }).catch(err => console.log(err))
 };
 
@@ -44,9 +42,7 @@ function getCODE(link) {
       //console.log(href);
       let code = href.replace("https://www.merinfo.se/redirect/lonekollen/", "");
       //console.log(code)
-      info.push({
-          "code": code
-      });
+      info."code": code;
       getAPI(code);
   }).catch(err => console.log(err))
 };
@@ -59,12 +55,8 @@ function getAPI(code) {
       console.log(data);
       //console.log(data["data"]["url"]);
       
-      info.push(
-          "car count": data["data"]["count"]
-      );
-      info.push(
-          "car link": data["data"]["url"]
-      );
+      info."car count": data["data"]["count"];
+      info."car link": data["data"]["url"];
       console.log(info);
   }).catch(err => console.log(err))
 };
