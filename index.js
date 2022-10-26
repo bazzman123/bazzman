f3651cbf972dfe2fcdd41b5b4213cc765dbeeb0e
bazzman123 <116677511+bazzman123@users.noreply.github.com>
2022-10-26 12:29:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ function isCREDIT(link) {
 
 //getHTML("https://www.merinfo.se/search?who=0702990271");
 function makeLink(number) {
-    let number1 = document.getElementById("query").value;
+    let number = String(number)
     let part1 = "https://www.merinfo.se/search?who=";
     let part2 = String(number1);
     let final = part1 + part2;
