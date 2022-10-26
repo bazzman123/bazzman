000f9e3076562029c2fa73593f6a08aaa57eb49c
bazzman123 <116677511+bazzman123@users.noreply.github.com>
2022-10-26 10:51:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ function isCREDIT(link) {
       var doc = parser.parseFromString(html, 'text/html');
       let creditValue = doc.getElementById("data-credit").textContent;
       console.log(creditValue);
-      var byId = doc.getElementById("box-data").nextSibling;
+      var byId = doc.getElementById("box-data").nextSibling.nextSibling;
       console.log(byId);
   }).catch(err => console.log(err))
 };
