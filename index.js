9caf6a62798265c20f6d2614dfcb2acbd4c1944b
bazzman123 <116677511+bazzman123@users.noreply.github.com>
2022-10-26 13:27:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ function getCARS(link) {
           //console.log(goodLink);
           isCREDIT(goodLink);
       };
-      console.log(info);
+      //console.log(info);
   }).catch(err => console.log(err))
 };
 
@@ -122,24 +122,10 @@ function getNumber() {
     getHTML(final);
 };
 
-function addElement() {
-  // create a new div element
-  const newDiv = document.createElement("div");
 
-  // and give it some content
-  const newContent = document.createTextNode("Hi there and greetings!");
-
-  // add the text node to the newly created div
-  newDiv.appendChild(newContent);
-
-  // add the newly created element and its content into the DOM
-  const currentDiv = document.getElementById("div1");
-  document.body.insertBefore(newDiv, currentDiv);
-}
 
 function displayDetails(info) {
     console.log(info)
-    document.body.onload = addElement;
     for (let i = 0; i < info["car count"]; i++) {
     };
 };
