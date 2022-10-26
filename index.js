c9dcdb53b6bf27adb67db0ba9dc0909e12b7412e
bazzman123 <116677511+bazzman123@users.noreply.github.com>
2022-10-26 09:54:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -74,8 +74,8 @@ function getCARS(link) {
       for (let i = 0; i < 3; i++) {
           //console.log(carLinks[i].innerHTML);
           let badLink = carLinks[i].firstElementChild.firstElementChild.href;
-          //console.log(badLink.substr(badLink.length-21));
-          let goodLink = "https://biluppgifter.se/brukare/person/" + badLink;
+          let goodPart = badLink.substr(badLink.length-21);
+          let goodLink = "https://biluppgifter.se/brukare/person/" + goodPart;
           console.log(goodLink);
       };
   }).catch(err => console.log(err))
